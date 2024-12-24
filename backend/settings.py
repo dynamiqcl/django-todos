4f3c602525857a8490cf6c01eff2f321b14b68d7
ignaciotapia1997 <ignacio.tapia1997@gmail.com>
2024-12-24 20:11:12 -0300

    Add CSRF trusted origins

--- backend/settings.py
+++ backend/settings.py
@@ -137,6 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Configuración de CORS para permitir solicitudes desde el frontend
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # Dirección del servidor Next.js
+    "https://django-todos-production.up.railway.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
